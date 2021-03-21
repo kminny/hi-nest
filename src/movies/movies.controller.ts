c5096bcf3609292c3924066c60c3b99867c0205b
kminny <darkdevil94@naver.com>
2021-03-21 16:42:31 +0900

    Controllers & Service generated by using nest cli

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return this.movieService.update(movieId, updateData);
+  }
+}
